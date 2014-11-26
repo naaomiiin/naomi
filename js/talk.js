549c52b7dcb017c3f7673d46274618afc9169c83
Naomi Ito <naaomiiiin@itou-no-MacBook-Air-2.local>
2014-11-26 19:22:40 +0900

    1126進捗

--- js/talk.js
+++ js/talk.js
@@ -4,6 +4,16 @@ $("#input").val("merci");
 console.log($("#input").val());*/
 
 $("#append-text").click(function(){
-	$("#history").append("<div>"+$("#input").val()+"</div>");
+
+	$("#history").append('<div class="question_Box">'
+			     +'<div class="question_image">'
+			     +'<img src="./girl.png" alt="user" width="160" height="160"/>'+'</div>'
+			     +'<div class="arrow_question">'+'<div id="history">'+'</div>'
+			     +'<div>'+$("#input").val()+'<div>'+'</div>'+'</div>'
+			    );
 	$("#input").val("");
-});
+    });
+
+
+
+	
