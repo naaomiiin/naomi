1c0c1caa9b7b0e75be0908ebb86be4ff2309f757
Naomi Ito <naaomiiiin@itou-no-MacBook-Air-2.local>
2014-11-29 13:22:28 +0900

    1129進捗

--- js/talk.js
+++ js/talk.js
@@ -7,7 +7,7 @@ $("#append-text").click(function(){
 
 	$("#history").append('<div class="question_Box">'
 			     +'<div class="question_image">'
-			     +'<img src="./girl.png" alt="user" width="160" height="160"/>'+'</div>'
+			     +'<img src="./girl.png" alt="user" width="130" height="130"/>'+'</div>'
 			     +'<div class="arrow_question">'+'<div id="history">'+'</div>'
 			     +'<div>'+$("#input").val()+'<div>'+'</div>'+'</div>'
 			    );
