0ccf7cbc68b3c2c97afe1837847d9f0426a08771
Naomi Ito <naaomiiiin@itou-no-MacBook-Air-2.local>
2014-12-03 14:21:38 +0900

    1202進捗

--- js/talk.js
+++ js/talk.js
@@ -5,15 +5,29 @@ console.log($("#input").val());*/
 
 $("#append-text").click(function(){
 
-	$("#history").append('<div class="question_Box">'
-			     +'<div class="question_image">'
-			     +'<img src="./girl.png" alt="user" width="130" height="130"/>'+'</div>'
-			     +'<div class="arrow_question">'+'<div id="history">'+'</div>'
-			     +'<div>'+$("#input").val()+'<div>'+'</div>'+'</div>'
-			    );
+	$("#history").append('<div class="container">'
+			     +'<div class="row col-sm-10">'
+			     +'<div class="col1">'
+			     +'<div class="question_Box">'
+			     +'<div class="question_image col-sm-3">'
+			     +'<img src="./girl.png" alt="user">'
+			     +'</div>'<!-- /.question_image -->
+			     +'<div class="arrow_question col-sm-9">'
+			     +'<div id="history">'
+			     +'</div>'<!-- /.history -->
+			     +'<div>'
+			     +$("#input").val()
+			     +'<br>'
+			     +'<div>'
+			     +'</div>'
+			     +'</div>'
+			     +'</div>'<!-- /.arrow_question -->
+			     +'</div>'<!-- /.question_Box -->
+			     +'</div>'<!-- /.col1 -->
+			     +'</div>'<!-- /.row -->
+			     +'</div>'<!-- /.container -->
+			     );
 	$("#input").val("");
     });
 
 
-
-	
